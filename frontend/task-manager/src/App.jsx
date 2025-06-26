cec08759bd46ff6d2ed756c2ad040c95771d7b22
pokaljainam24 <pokaljainam@gmail.com>
2025-06-26 15:42:35 +0530

    done

--- frontend/task-manager/src/App.jsx
+++ frontend/task-manager/src/App.jsx
@@ -14,7 +14,7 @@ import SignUp from './pages/Auth/SignUp';
 import ManageTasks from './pages/Admin/ManageTasks';
 import CreateTask from './pages/Admin/CreateTask';
 import ManageUsers from './pages/Admin/ManageUsers';
-
+      
 import UserDashboard from './pages/User/UserDashboard';
 import MyTasks from './pages/User/MyTasks';
 import ViewTaskDetails from './pages/User/ViewTaskDetails';
